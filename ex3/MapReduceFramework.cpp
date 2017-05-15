1469d7af9d7bacf04ed5de845a59a2c35123d9f8
Shiri Heffetz <h3shiri@gmail.com>
2017-05-15 22:06:04 +0300

    basic functionality is working :) including our lovely semaphore.

--- ex3/MapReduceFramework.cpp
+++ ex3/MapReduceFramework.cpp
@@ -13,6 +13,8 @@
 #define FAILURE -1
 #define KRED  "\x1B[31m"
 #define KMAG  "\x1B[35m"
+#define RST  "\x1B[0m"
+
 
 using namespace std;
 /**
@@ -38,58 +40,73 @@ pthread_mutex_t reduceIndexMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t retrivalIndexMutex = PTHREAD_MUTEX_INITIALIZER;
 
 /**
- * Protecting the int numOfMapThreadFinished.
+ * simple init for the reduce threads.
  */
-pthread_mutex_t numOfMapThreadsFinishedMutex = PTHREAD_MUTEX_INITIALIZER;
-int numOfMapThreadFinished = 0;
+pthread_mutex_t initReduceThreadsMutex = PTHREAD_MUTEX_INITIALIZER;
+
+/**
+ * Protecting the map threads counter
+ */
+pthread_mutex_t protectMapCounter = PTHREAD_MUTEX_INITIALIZER;
+
+/**
+ * counting the number of Map User level threads that finished.
+ */
+static int mapThreadCounter = 0;
 
 /**
  * index of next <k3Base*,list<v3Base*>> to take by next execReduce thread from
  * shuffleOutput
  */
-int reduceIndex = 0;
+static int reduceIndex = 0;
 
 /**
  * index in k1v1bec to take a chunk from by execMap threads
  */
-int retrivalIndex = 0;
+static int retrivalIndex = 0;
 
 /**
  * map of pthread_t: mapThreadData
  */
-map<pthread_t, mapThreadData> k2v2Map;
+static map<pthread_t, mapThreadData> k2v2Map;
 
 /**
  * map reduce to use by the map and reduce threads
  */
-MapReduceBase * mapReduceToUse = nullptr;
+static MapReduceBase * mapReduceToUse = nullptr;
 
 
 /**
  * vector contain pairs of <k1Base*,v1Base*> to deal with.
  * aka our input from the user.
  */
-IN_ITEMS_VEC k1v1Vec = vector<IN_ITEM>();
+static IN_ITEMS_VEC k1v1Vec = vector<IN_ITEM>();
 
 /**
  * tells if the framework is responsible to free k2v2 values
  */
-bool k2v2memoryResponse;
+static bool k2v2memoryResponse;
 
 /**
  * map for the shuffles output
  */
-map<k2Base*, vector<v2Base*>> shuffleOutPut;
+static map<k2Base*, vector<v2Base*>> shuffleOutPut = map<k2Base*, vector<v2Base*>>();
 
 /**
  * map for the execReduceThreads
  */
-map<pthread_t, std::list<pair<k3Base*,v3Base*>>> reduceOutput;
+static map<pthread_t, std::list<pair<k3Base*,v3Base*>>> reduceOutput =
+        map<pthread_t, std::list<pair<k3Base*,v3Base*>>>();
 
 /**
- * number of userLevelThreads we defined =.
+ * indicates that execMap threads finished
  */
-int threadsNum = 0;
+static bool execMapFinished = false;
+
+/**
+ * * number of userLevelThreads we defined.
+ */
+static int totalThreads = 0;
 
 //functions declaretions:
 void *  execMapRutine(void* dummyArg);
@@ -104,18 +121,24 @@ void* execReduce(void* dummyArg);
 void initExecReduceThreads(int multiThreadLevel);
 void sortOutputAndExit(vector<OUT_ITEM> * retVal);
 bool comparator(pair<k3Base*, v3Base*> p1, pair<k3Base*, v3Base*> p2);
+
 void printLikeAManiac(string eMsg);
-void printSamVal();
+void testingShuffleOutputs();
 
 /**
  * initialize mutexes and semaphore that will be used
  */
 void initMutexSemaphore()
 {
+	//TODO: problematic having no credit for the shuffle.
     if((sem_init(&shuffleSem, 0, 1)))
     {
         errorDetect("sem_init");
     }
+    //TODO: remove testing prints
+//    int val;
+//    sem_getvalue(&shuffleSem,&val);
+//    cout << "init" << val << std::endl;
 }
 
 
@@ -123,7 +146,7 @@ void mapAndShuffle(int multiThreadLevel)
 {
     //initialize threads
     lock(&k2v2MapMutex);
-    for(int i=0; i < multiThreadLevel; ++i)
+    for(int i=0; i < multiThreadLevel;++i)
     {
         pthread_t thread;
         if(pthread_create(&thread, nullptr, &execMapRutine, nullptr) != 0)
@@ -132,23 +155,34 @@ void mapAndShuffle(int multiThreadLevel)
         }
         k2v2Map[thread] = mapThreadData();
     }
+    //shuffle
+    pthread_t shuffle;
+    if(pthread_create(&shuffle,NULL, shuffleRutine,NULL) != 0)
+    {
+        errorDetect("pthread_create");
+    }
     unlock(&k2v2MapMutex);
-	//shuffle
-	pthread_t shuffle;
-	if(pthread_create(&shuffle,NULL, shuffleRutine,NULL) != 0)
-	{
-		errorDetect("pthread_create");
-	}
+    for(map<pthread_t, mapThreadData>::iterator it = k2v2Map.begin();
+        it!=k2v2Map.end(); ++it)
+    {
+        if (pthread_join((*it).first, NULL) != 0)
+        {
+            errorDetect("pthread_join");
+        }
+    }
+    if(sem_post(&shuffleSem) !=0)
+    {
+        errorDetect("sem_post");
+    }
 	if(pthread_join(shuffle, NULL) != 0)
 	{
 		errorDetect("pthread_join");
 	}
-
 }
 
 /**
  *
- * @param dummyArg - only to use properly as startrutine
+ * @param dummyArg - only to use properly as start routine
  * argument to pthread_create
  * @return NULL
  */
@@ -159,14 +193,15 @@ void * execMapRutine(void* dummyArg)
     lock(&k2v2MapMutex);
     unlock(&k2v2MapMutex);
     int elementsNum = k1v1Vec.size();
-	int mySIndex = 0;
+    int mySIndex = 0;
 	int myEIndex = 0;
-	int chunkSize = 0;
+	int chunkSize;
     while(true)
     {
         lock(&retrivalIndexMutex);
 		if(retrivalIndex != elementsNum)
 		{
+//            cout << KRED << pthread_self() << endl;
 			mySIndex = retrivalIndex;
 			chunkSize = min(CHUNK_SIZE, elementsNum  - retrivalIndex);
 			retrivalIndex  = retrivalIndex + chunkSize;
@@ -184,13 +219,15 @@ void * execMapRutine(void* dummyArg)
 			mapReduceToUse->Map(k1v1.first, k1v1.second);
 		}
     }
-    lock(&numOfMapThreadsFinishedMutex);
-    numOfMapThreadFinished++;
-    unlock(&numOfMapThreadsFinishedMutex);
+    lock(&protectMapCounter);
+    mapThreadCounter++;
+    unlock(&protectMapCounter);
+    sem_post(&shuffleSem);
 }
 
 void initExecReduceThreads(int multiThreadLevel)
 {
+    lock(&initReduceThreadsMutex);
 	for(int i=0; i < multiThreadLevel;i++)
 	{
 		pthread_t thread;
@@ -200,7 +237,8 @@ void initExecReduceThreads(int multiThreadLevel)
 		}
 		reduceOutput[thread] = list<OUT_ITEM>();
 	}
-	for(map<pthread_t, std::list<OUT_ITEM>>::iterator it = reduceOutput.begin();
+    unlock(&initReduceThreadsMutex);
+    for(map<pthread_t, std::list<OUT_ITEM>>::iterator it = reduceOutput.begin();
 	it!=reduceOutput.end();++it)
 	{
 		if (pthread_join((*it).first, NULL) != 0)
@@ -211,39 +249,46 @@ void initExecReduceThreads(int multiThreadLevel)
 
 }
 
-
+// TODO: solve Problematic logic causes infinite loop.
 void* execReduce(void* dummyArg)
 {
+    lock(&initReduceThreadsMutex);
+    unlock(&initReduceThreadsMutex);
 	int elementsNum = shuffleOutPut.size();
 	int mySIndex = 0;
 	int myEIndex = 0;
-	int i = 0;
-	int chunkSize;
-	map<k2Base*, vector<v2Base*>>::iterator iter; ;
+	int chunkStep;
 
 	while(true)
 	{
 		lock(&reduceIndexMutex);
-		if(reduceIndex != elementsNum)
+		if(reduceIndex < elementsNum)
 		{
 			mySIndex = reduceIndex;
-			i = reduceIndex;
-			chunkSize = min(CHUNK_SIZE, elementsNum  - reduceIndex);
-			retrivalIndex  = reduceIndex + chunkSize;
+			chunkStep = min(CHUNK_SIZE, elementsNum  - reduceIndex);
+			myEIndex  = reduceIndex + chunkStep; // aka where to start next iteration.
+			reduceIndex = myEIndex;
 			unlock(&reduceIndexMutex);
-			myEIndex = mySIndex + reduceIndex - 1;
 		}
+		// In case there are no more elements to iterate over.
 		else
 		{
 			unlock(&reduceIndexMutex);
 			break;
 		}
-		iter = std::next(shuffleOutPut.begin(), mySIndex);
-		while( i  <= myEIndex)
+		// should potentially start iterating from a certain point.
+        map<k2Base*, vector<v2Base*>>::iterator iter;
+        iter = std::next(shuffleOutPut.begin(), mySIndex);
+        /* iterating over the val2 elements in the vector */
+		for (int i = mySIndex; i < myEIndex; ++i)
 		{
-			k2Base * k2  = (*iter).first;
-			mapReduceToUse->Reduce(k2, (*iter).second);
-			if(k2v2memoryResponse)
+			const k2Base * k2  = (*iter).first;
+			//Copying the vector should protect us from some issues.
+			const vector<v2Base*> v{ begin((*iter).second), end((*iter).second) };
+			mapReduceToUse->Reduce(k2, v);
+			//TODO: remove deletion to a more secure location it might causes some errors.
+            //due to the fact several threads are trying to mess with the same data structure on the same time.
+            if(k2v2memoryResponse)
 			{
 				delete k2;
 				k2 = NULL;
@@ -253,12 +298,10 @@ void* execReduce(void* dummyArg)
 					delete *it;
 				}
 			}
-			(*iter).second.clear();
+//			(*iter).second.clear();
 			++iter;
-			++i;
 		}
 	}
-
 }
 
 
@@ -266,75 +309,65 @@ void* execReduce(void* dummyArg)
 
 void* shuffleRutine(void* dummyArg)
 {
-	//treat semaphore
-	int length = 0;
-	int treatedPairs = 0;
-	bool chosenk2p;
-	int internalCount = 0;
-	int pairsNum = k1v1Vec.size();
+	//TODO: treat semaphore?
+    int length = 0;
+//	int treatedPairs = 0;
+	bool choosenk2p;
 	while(true) {
-		for (std::map<pthread_t, mapThreadData>::iterator it1=k2v2Map.begin();
+        //TODO: clean this function
+        /* this is the outer layer iterator */
+        for (std::map<pthread_t, mapThreadData>::iterator it1=k2v2Map.begin();
 			 it1 != k2v2Map.end(); ++it1)
 		{
-//			sem_wait(&shuffleSem);
 			lock(&(*it1).second._itemsMutex);
 			vector <k2v2pair>* itemsp = &(*it1).second._k2v2items;
 			length = itemsp->size();
-			internalCount = 0;
-			for (int i = 0; i < length; i++) {
-				k2Base *k = (*itemsp).at(i).first;
-				v2Base *v = (*itemsp).at(i).second;
+            while(length)
+            {
+				k2Base *k = (*itemsp).at(length-1).first;
+				v2Base *v = (*itemsp).at(length-1).second;
 				for (auto& it : shuffleOutPut) {
-					chosenk2p = false;
+					choosenk2p = false;
 					if (equal(*(it.first), (*k))) {
-						shuffleOutPut[(it.first)].push_back((*itemsp)[i].second);
-						chosenk2p = true;
+						shuffleOutPut[(it.first)].push_back(v);
+						choosenk2p = true;
 						if (k2v2memoryResponse) {
 							//to check if done properly
 							delete k;
 							k = NULL;
 						}
 						//to write the position and use erase
-						itemsp->erase(itemsp->begin()+ i);
-						treatedPairs++;
-						internalCount++;
-						//down semaphore
+						itemsp->erase(itemsp->begin()+ length-1);
+//						treatedPairs++;
+                        length--;
 						break;
 					}
 				}
-				if (!chosenk2p) {
-					shuffleOutPut[(*itemsp)[i].first] = vector<v2Base *>();
-					(shuffleOutPut[(*itemsp)[i].first]).push_back((*itemsp)[i].second);
-					itemsp->erase(itemsp->begin() + i);
-					treatedPairs++;
-					internalCount++;
-					//itemsp->remove(items[i]);
-					//down semaphore
+				if (!choosenk2p) {
+					shuffleOutPut[(*itemsp)[length-1].first] = vector<v2Base *>();
+					(shuffleOutPut[(*itemsp)[length-1].first]).push_back((*itemsp)[length-1].second);
+					itemsp->erase(itemsp->begin() + length-1);
+//                    treatedPairs++;
+                    length--;
 				}
 
-
-			}
+            }
 			unlock(&(*it1).second._itemsMutex);
-			if((itemsp->size()!= 0)|| (internalCount != (length)))
-			{
-				std::cout <<"list is not empty after shuffle treat!" << endl;
-			}
 		}
-        lock(&numOfMapThreadsFinishedMutex);
-        if (numOfMapThreadFinished == threadsNum)
-        {
+        //WHY: treatedPairs == 0 ??
+        if(execMapFinished) {
+            //TEST: test extra iteration
             break;
         }
-        unlock(&numOfMapThreadsFinishedMutex);
-//		if(treatedPairs == pairsNum)
-//		{
-//			break;
-//		}
-	}
-    unlock(&numOfMapThreadsFinishedMutex);
+        lock(&protectMapCounter);
+        if (mapThreadCounter >= totalThreads){
+            execMapFinished = true;
+        }
+        unlock(&protectMapCounter);
+        sem_wait(&shuffleSem);
+    }
+//    testingShuffleOutputs();
 }
-
-
 
 /**asystem call fail
  *
@@ -374,8 +407,8 @@ void unlock(pthread_mutex_t* toUnlock)
 OUT_ITEMS_VEC RunMapReduceFramework(MapReduceBase& mapReduce, IN_ITEMS_VEC& itemsVec,
                                     int multiThreadLevel, bool autoDeleteV2K2)
 {
-    threadsNum = multiThreadLevel;
-    //treat case of vetorSize = 0 i.e no items to map
+	totalThreads = multiThreadLevel;
+    //TODO: treat case of vetorSize = 0, i.e no items to map
 	mapReduceToUse = &mapReduce;
     k1v1Vec = itemsVec;
     k2v2memoryResponse = autoDeleteV2K2;
@@ -384,6 +417,7 @@ OUT_ITEMS_VEC RunMapReduceFramework(MapReduceBase& mapReduce, IN_ITEMS_VEC& item
 	initExecReduceThreads(multiThreadLevel);
 	vector<OUT_ITEM> * retVal = new vector<OUT_ITEM>();
 	sortOutputAndExit(retVal);
+    return *retVal;
 }
 
 void Emit2 (k2Base* k2, v2Base* v2)
@@ -392,9 +426,12 @@ void Emit2 (k2Base* k2, v2Base* v2)
 	lock(&k2v2Map.at(id)._itemsMutex);
 	k2v2Map.at(id)._k2v2items.push_back(std::make_pair(k2,v2));
 	unlock(&k2v2Map.at(id)._itemsMutex);
-//	sem_post(&shuffleSem);
+//    TEST1: turning off the sem_shuffle.
+	if(sem_post(&shuffleSem) != 0)
+    {
+        errorDetect("sem_post");
+    }
 }
-
 
 bool equal(const k2Base& fk1, const k2Base& fk2)
 {
@@ -417,7 +454,11 @@ void sortOutputAndExit(vector<OUT_ITEM> * retVal)
 	for (map<pthread_t, std::list<pair<k3Base*,v3Base*>>>::iterator
                  it = reduceOutput.begin(); it != reduceOutput.end(); ++it)
 	{
-		temp = { it->second.begin(), it->second.begin()};
+		temp = { begin(it->second), end(it->second)};
+        //TODO: remove print later.
+        if(temp.size() == 0){
+            continue;
+        }
 		retVal->insert(retVal->end(), temp.begin(), temp.end());
 	}
 	sort(retVal->begin(), retVal->end(), comparator);
@@ -441,4 +482,12 @@ void printSamVal(){
 	int * val = nullptr;
 	sem_getvalue(&shuffleSem, val);
 	cout << KMAG << val << endl;
+}
+
+void testingShuffleOutputs() {
+    printLikeAManiac("endOfshuffle");
+    for (map<k2Base*, vector<v2Base*>>::iterator iterLOL = shuffleOutPut.begin(); iterLOL != shuffleOutPut.end(); ++iterLOL)
+    {
+        cout << RST << iterLOL->second.size() << endl;
+    }
 }
